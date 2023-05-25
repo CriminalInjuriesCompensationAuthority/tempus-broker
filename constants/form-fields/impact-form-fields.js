76cfff782c2075949b19099d0c15507df807d0e9
msenkiw <michael.senkiw@informed.com>
2023-05-25 12:01:51 +0100

    Initial mapping of json to oracle insert statement

---
+++ constants/form-fields/impact-form-fields.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = Object.freeze({
+    work_situation: ['q-applicant-work-details-option', 'q-applicant-job-when-crime-happened']
+});
---
+++ constants/form-fields/treatment-form-fields.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = Object.freeze({
+    gp_name: 'q-gp-organisation-name',
+    registered_with_gp: 'q-applicant-are-you-registered-with-gp',
+    registered_gp_attended: 'q-applicant-have-you-seen-a-gp'
+});
