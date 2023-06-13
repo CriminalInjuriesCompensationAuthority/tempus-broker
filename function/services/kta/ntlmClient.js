07923a9bd50585a478adc127acf11b90ba664623
jack-burt-is <61201138+jack-burt-is@users.noreply.github.com>
2023-06-13 11:17:09 +0000

    CDBT-84 - KTA Integration (#19)
    * Start integrating with KTA service
    
    * Add tests
    
    * Start integrating with KTA service
    
    * Add tests
    
    * Fix failin test
    
    * Await and catch

---
+++ function/services/kta/ntlmClient.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const httpNtlmClient = require('httpntlm');
+const getParameter = require('../ssm/index');
+const getSecret = require('../secret-manager/index');
+
+const client = {
+    async sendRequest(serviceUrl, jsonRequest) {
+        const ktaArn = await getParameter('kta-secret-arn');
+        const ktaSecret = JSON.parse(await getSecret(ktaArn));
+
+        return new Promise(function(resolve, reject) {
+            const opts = {
+                username: ktaSecret.username,
+                password: ktaSecret.password,
+                domain: ktaSecret.domain,
+                workstation: '',
+                url: ktaSecret.sdkPrefix + serviceUrl,
+                json: jsonRequest
+            };
+
+            httpNtlmClient.post(opts, function(err, response) {
+                if (err) {
+                    reject(err);
+                } else {
+                    const parsedResponse = JSON.parse(response.body);
+                    if (parsedResponse.Message) {
+                        reject(parsedResponse.Message);
+                    } else {
+                        resolve(parsedResponse.d);
+                    }
+                }
+            });
+        });
+    }
+};
+
+module.exports = client;
