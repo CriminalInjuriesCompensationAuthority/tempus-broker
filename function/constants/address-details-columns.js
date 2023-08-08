313e212d78ce227f64550ace7efacde917a124d8
msenkiw <michael.senkiw@informed.com>
2023-08-08 10:38:26 +0100

    delete split fatality column

--- function/constants/address-details-columns.js
+++ function/constants/address-details-columns.js
@@ -60,9 +60,6 @@ module.exports = Object.freeze({
         'q-rep-postcode': 'post_code'
     },
     DCA: {
-        'q-deceased-title': 'dec_title',
-        'q-deceased-first-name': 'dec_first_name',
-        'q-deceased-last-name': 'dec_last_name',
         'q-deceased-building-and-street': 'address_line_1',
         'q-deceased-building-and-street-2': 'address_line_2',
         'q-deceased-building-and-street-3': 'address_line_3',
