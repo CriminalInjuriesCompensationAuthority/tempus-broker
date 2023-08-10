90d2b18f090dea700cd9af46848e6d2558cfcd96
jack-burt-is <jack.burt@informed.com>
2023-08-10 15:27:02 +0000

    Switched to schedule trigger with querying of SQS queue

--- function/index.test.js
+++ function/index.test.js
@@ -1,32 +1,18 @@
 'use strict';
 
 const fs = require('fs');
-const {mockClient} = require('aws-sdk-client-mock');
-const {SSMClient, GetParameterCommand} = require('@aws-sdk/client-ssm');
-const index = require('./index');
+const {handler, handleTempusBrokerMessage} = require('./index');
 
 describe('Tempus broker function', () => {
     it.skip('Should run the function handler', async () => {
-        const eventFile = fs.readFileSync('function/resources/testing/event.json');
-        const event = JSON.parse(eventFile);
-        const response = await index.handler(event, null);
+        const response = await handler({}, null);
         expect(response).toContain('Success!');
     });
 
     it('Should error if event body contains files with invalid types', async () => {
-        const mockSSMClient = mockClient(SSMClient);
-        const eventFile = fs.readFileSync(
-            'function/resources/testing/event-with-invalid-files.json'
-        );
-        const event = JSON.parse(eventFile);
-
-        mockSSMClient.on(GetParameterCommand).resolves({
-            Parameter: {
-                value: 'test'
-            }
-        });
-        await expect(async () => index.handler(event, null)).rejects.toThrowError(
-            'Tempus broker queue message held an invalid file type, only .pdf and .json are supported'
-        );
+        const sqsMessage = fs.readFileSync('function/resources/testing/sqsMessage.json');
+        const response = handleTempusBrokerMessage(JSON.parse(sqsMessage).Messages[0].Body);
+        expect(Object.keys(response)).toContain('applicationJSONDocumentSummaryKey');
+        expect(Object.keys(response)).toContain('applicationPDFDocumentSummaryKey');
     });
 });
