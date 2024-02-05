1240807b666ffca1eaaf782a45c3d91714901ec4
Stephen S <Stephen.Sinclair@CICA.GOV.UK>
2024-02-05 14:57:39 +0000

    Modify function handler test to clean up test data

--- function/index.test.js
+++ function/index.test.js
@@ -1,10 +1,33 @@
 'use strict';
 
+const oracledb = require('oracledb');
 const fs = require('fs');
 const {handler, handleTempusBrokerMessage} = require('./index');
+const createDBPool = require('./db/dbPool');
+
+async function deleteAnyPreviousTestData() {
+    const dbConn = await createDBPool();
+    const connection = await oracledb.getConnection('TempusBrokerPool');
+    await connection.execute(
+        `DELETE FROM ADDRESS_DETAILS WHERE CLAIM_REFERENCE_NUMBER = '327507'`,
+        {},
+        {autoCommit: true}
+    );
+    await connection.execute(
+        `DELETE FROM APPLICATION_FORM WHERE CLAIM_REFERENCE_NUMBER = '327507'`,
+        {},
+        {autoCommit: true}
+    );
+
+    if (dbConn) {
+        await oracledb.getPool('TempusBrokerPool').close(0);
+    }
+}
 
 describe('Tempus broker function', () => {
     it.skip('Should run the function handler', async () => {
+        await deleteAnyPreviousTestData();
+
         jest.setTimeout(60000);
         const response = await handler({}, null);
         expect(response).toContain('Success!');
