d7dcfa0fdd41269df53959d6ab8450ff6a013955
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2025-03-10 16:37:42 +0000

    Further PR changes

--- function/index.js
+++ function/index.js
@@ -54,15 +54,13 @@ function getAnswerFromThemes (themes, themeId, answerId) {
 function getApplicationOrigin(applicationData){
     const testEmails = process.env.TEST_EMAILS || '';
 
-    const themesArray = applicationData.themes || [];
-
     const emailAddresses = [
-        getAnswerFromThemes(themesArray, 'applicant-details', 'q-applicant-enter-your-email-address'),
-        getAnswerFromThemes(themesArray, 'main-applicant-details', 'q-mainapplicant-enter-your-email-address'),
-        getAnswerFromThemes(themesArray, 'rep-details', 'q-rep-email-address')
+        getAnswerFromThemes(applicationData.themes, 'applicant-details', 'q-applicant-enter-your-email-address'),
+        getAnswerFromThemes(applicationData.themes, 'main-applicant-details', 'q-mainapplicant-enter-your-email-address'),
+        getAnswerFromThemes(applicationData.themes, 'rep-details', 'q-rep-email-address')
     ];
 
-    return emailAddresses.some(email => email && testEmails.includes(email)) ? "internal" : "external";
+    return emailAddresses.some(email => email && testEmails.includes(email)) ? 'internal' : 'external';
 }
 
 async function handler(event, context) {
@@ -101,8 +99,7 @@ async function handler(event, context) {
         );
 
         const maintenanceMode = process.env.MAINTENANCE_MODE === 'true';
-
-        // Return early if the service is in maintenance mode and the traffic is external.
+        
         if (maintenanceMode && getApplicationOrigin(s3ApplicationData) === 'external') {
             logger.info('External traffic received. Maintenance mode is on.');
             return 'Nothing to process';
